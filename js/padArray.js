786fea872199582ea690263cba05c6aed5e457a9
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-29 13:30:23 -0500

    Js Solved

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,11 @@
 // REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
-
+exports.paded = function pad(array, minSize, value=null){
+    if(minSize <= array.length){
+        return array
+    } else {
+        while(array.length != minSize){
+            array.push(value)
+        }
+    }
+    return array
 }
--- js/padArraySpec.js
+++ js/padArraySpec.js
@@ -1,1 +1,5 @@
-// Write unit tests!
+var pad = require("./padArray");
+
+console.log(pad.paded([1,2,3], 5, 'apple'))
+console.log(pad.paded([1,2,3],5))
+console.log(pad.paded([1,2,3,4],3,'cheese'))
